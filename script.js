62fb808eb3991573a53bbc206093d09daee045bf
zamilnoushad5-ux <zamilnoushad5@gmail.com>
2026-09-22 23:35:31 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-const SUPABASE_URL = "https://jkfwjfkszaitugduuamz.supabase.co/rest/v1/";
-const SUPABASE_KEY = "sb_publishable_Zpy-a6SPlOonHVEYKPJEaQ_FbHsBCby";
-const supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
+
 const branches = [
   {
     id: "souq6",
